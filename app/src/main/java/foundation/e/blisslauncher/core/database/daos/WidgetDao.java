e6181b4e99216d3f1a67e4454a76090e2b182ddd
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-04-12 12:58:59 +0700

    Insert default widgets at the top

--- app/src/main/java/foundation/e/blisslauncher/core/database/daos/WidgetDao.java
+++ app/src/main/java/foundation/e/blisslauncher/core/database/daos/WidgetDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -15,8 +16,14 @@ public interface WidgetDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     long insert(WidgetItem widgetItem);
 
+    @Query("UPDATE widget_items SET height = :height WHERE id = :id")
+    void updateHeight(int id, int height);
+
     @Query("SELECT height FROM widget_items WHERE id = :id")
     int getHeight(int id);
+
+    @Query("SELECT * FROM widget_items")
+    List<WidgetItem> getAll();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(List<WidgetItem> widgetItems);
--- app/src/main/java/foundation/e/blisslauncher/core/database/model/WidgetItem.java
+++ app/src/main/java/foundation/e/blisslauncher/core/database/model/WidgetItem.java
@@ -1,22 +1,28 @@
 package foundation.e.blisslauncher.core.database.model;
 
-
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "widget_items")
 public class WidgetItem {
 
+    public static final int DEFAULT_ORDER = 99999;
+
     @PrimaryKey
     public int id;
-    public int height;
 
-    public WidgetItem(){
+    @ColumnInfo(defaultValue = "0")
+    public int height = 0;
+
+    @ColumnInfo(defaultValue = "99999")
+    public int order = DEFAULT_ORDER;
+
+    public WidgetItem() {
 
     }
 
-    public WidgetItem(int id, int height){
+    public WidgetItem(int id) {
         this.id = id;
-        this.height = height;
     }
 }
