5e0f444a5eb9928d51c9d2ffa69583fb23c5457e
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-05-23 09:33:31 +0700

    Respect global notification dots settings

--- app/src/main/java/foundation/e/blisslauncher/BlissLauncher.java
+++ app/src/main/java/foundation/e/blisslauncher/BlissLauncher.java
@@ -1,7 +1,10 @@
 package foundation.e.blisslauncher;
 
+import static foundation.e.blisslauncher.util.SettingsCache.NOTIFICATION_BADGING_URI;
+
 import android.app.Application;
 import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 
 import foundation.e.blisslauncher.core.DeviceProfile;
@@ -9,6 +12,8 @@ import foundation.e.blisslauncher.core.IconsHandler;
 import foundation.e.blisslauncher.core.blur.BlurWallpaperProvider;
 import foundation.e.blisslauncher.core.customviews.WidgetHost;
 import foundation.e.blisslauncher.features.launcher.AppProvider;
+import foundation.e.blisslauncher.features.notification.NotificationService;
+import foundation.e.blisslauncher.util.SettingsCache;
 
 public class BlissLauncher extends Application {
     private IconsHandler iconsPackHandler;
@@ -29,6 +34,18 @@ public class BlissLauncher extends Application {
 
         connectAppProvider();
         BlurWallpaperProvider.Companion.getInstance(this);
+
+        SettingsCache settingsCache = SettingsCache.INSTANCE.get(this);
+        SettingsCache.OnChangeListener notificationLister = this::onNotificationSettingsChanged;
+        settingsCache.register(NOTIFICATION_BADGING_URI, notificationLister);
+        onNotificationSettingsChanged(settingsCache.getValue(NOTIFICATION_BADGING_URI));
+    }
+
+    private void onNotificationSettingsChanged(boolean areNotificationDotsEnabled) {
+        if (areNotificationDotsEnabled) {
+            NotificationService.requestRebind(new ComponentName(
+                    this, NotificationService.class));
+        }
     }
 
     public static BlissLauncher getApplication(Context context) {
--- app/src/main/java/foundation/e/blisslauncher/features/notification/NotificationService.java
+++ app/src/main/java/foundation/e/blisslauncher/features/notification/NotificationService.java
@@ -1,10 +1,15 @@
 package foundation.e.blisslauncher.features.notification;
 
+import static foundation.e.blisslauncher.util.SettingsCache.NOTIFICATION_BADGING_URI;
+
 import android.content.Intent;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
+import java.util.Collections;
+
 import foundation.e.blisslauncher.core.utils.ListUtil;
+import foundation.e.blisslauncher.util.SettingsCache;
 
 /**
  * Created by falcon on 14/3/18.
@@ -12,31 +17,68 @@ import foundation.e.blisslauncher.core.utils.ListUtil;
 
 public class NotificationService extends NotificationListenerService {
 
+    private static boolean sIsConnected = false;
+
     NotificationRepository mNotificationRepository;
+
+    private boolean mDotsEnabled;
+    private SettingsCache mSettingsCache;
+    private SettingsCache.OnChangeListener mNotificationSettingsChangedListener;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mNotificationRepository = NotificationRepository.getNotificationRepository();
+
+        // Register an observer to rebind the notification listener when dots are re-enabled.
+        mSettingsCache = SettingsCache.INSTANCE.get(this);
+        mNotificationSettingsChangedListener = this::onNotificationSettingsChanged;
+        mSettingsCache.register(NOTIFICATION_BADGING_URI,
+                mNotificationSettingsChangedListener);
+        onNotificationSettingsChanged(mSettingsCache.getValue(NOTIFICATION_BADGING_URI));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mSettingsCache.unregister(NOTIFICATION_BADGING_URI, mNotificationSettingsChangedListener);
+        mNotificationRepository.updateNotification(Collections.emptyList());
+    }
+
+    private void onNotificationSettingsChanged(boolean areNotificationDotsEnabled) {
+        mDotsEnabled = areNotificationDotsEnabled;
+        if (!areNotificationDotsEnabled && sIsConnected) {
+            requestUnbind();
+            updateNotifications();
+        }
     }
 
     @Override
     public void onListenerConnected() {
-        mNotificationRepository.updateNotification(ListUtil.asSafeList(getActiveNotifications()));
+        sIsConnected = true;
+        updateNotifications();
+    }
+
+    @Override
+    public void onListenerDisconnected() {
+        sIsConnected = false;
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        mNotificationRepository.updateNotification(ListUtil.asSafeList(getActiveNotifications()));
+        updateNotifications();
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
+        updateNotifications();
+    }
+
+    private void updateNotifications() {
+        if (!mDotsEnabled) {
+            mNotificationRepository.updateNotification(Collections.emptyList());
+            return;
+        }
         mNotificationRepository.updateNotification(ListUtil.asSafeList(getActiveNotifications()));
     }
 
