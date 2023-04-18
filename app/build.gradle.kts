aabb7bc7fd5463684089ca67c636524d222d43f6
Jonathan Klee <jonathan.klee@e.email>
2023-04-18 14:54:26 +0200

    Bump version for 1.11 beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
 
 // Manifest version information
 val versionMajor = 1
-val versionMinor = 10
-val versionPatch = 2
+val versionMinor = 11
+val versionPatch = 0
 
 val localProps = Properties()
 val localPropsFile = project.rootProject.file("local.properties")
